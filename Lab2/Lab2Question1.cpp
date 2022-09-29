aa7e384b4c9c58cc2411317d954b438d97724471
AvieRuby <aviecyc@gmail.com>
2022-09-29 18:47:55 +0800

    Finished Code

---
+++ Lab2/Lab2Question1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <new>
+using namespace std;
+
+struct Employee
+    {
+        string name;
+        float salary;
+    };
+
+int main()
+    {
+        Employee* DynArray;
+        int i , j , x = 1;
+
+        cout << "Enter number of employees: ";
+        cin >> i;
+
+        DynArray = new Employee[i];
+
+        for (j = 0; j < i; j++)  
+            {
+                cout << "\nEmployee: " << x << endl;
+                cout << "       Enter name: ";
+                cin >> DynArray[j].name;
+                cout << "       Enter salary: ";
+                cin >> DynArray[j].salary;
+
+                x++;
+            }
+
+
+        cout << "\n\n*****Employee Details*******\n" << endl;
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << "Name  : " << DynArray[j].name << endl;
+            cout << "Salary: " << DynArray[j].salary << endl << endl;       
+        }
+
+        delete[] DynArray;
+        return 0;
+    }
